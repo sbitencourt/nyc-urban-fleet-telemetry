4670213d25bc55efa8e19f6de0ae09cfed912652
Sarah Bitencourt <sarahcristinabitencourt17@gmail.com>
2026-02-06 08:20:43 -0300

    feat(pipeline) create a function to upload files into a S3 bucket, and create an empty python file for data ingestion in bronze layer

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -9,7 +9,17 @@ def get_bucket_name():
 def get_s3_client():
     """
     This function returns the S3 Client.
-    """
-
+    """    
     region_code = "us-east-2"
     return boto3.client('s3', region_name=region_code)
+
+    
+def upload_file_into_s3_bucket(s3_client, bucket_name: str, file_obj, folder_name: str, filename: str) -> None:
+    """
+    Upload a file-like object into an S3 bucket.
+    """
+    s3_client.upload_fileobj(
+        file_obj,
+        bucket_name,
+        f"{folder_name}/{filename}"
+    )
